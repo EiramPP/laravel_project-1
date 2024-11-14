14cb93b405d4921bb4d71ecee191ff61fc77d67a
EiramPP <lastjoseph030@gmail.com>
2024-11-14 11:24:48 +0800

    Update users.blade.php

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -8,8 +8,37 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <a href="{{ route('user.create')}}" >New User</a>
+                <div class="p-6 text-gray-1900">
+                    <a href="{{ route('user.create')}}" >Users</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-1">
+        <div class="max-w-6xl mx-auto sm:px-4 lg:px-3">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-4 text-gray-800">
+                    <img src="joseph.jpg" alt="">
+                    <a href="{{ route('user.create')}}" >joseph</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-1">
+        <div class="max-w-6xl mx-auto sm:px-4 lg:px-3">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-4 text-gray-800">
+                    <a href="{{ route('user.create')}}" >Users</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-1">
+        <div class="max-w-6xl mx-auto sm:px-4 lg:px-3">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-4 text-gray-800">
+                    <a href="{{ route('user.create')}}" >Users</a>
                 </div>
             </div>
         </div>
