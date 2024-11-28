7a2534b7ebe44f24472d3dc760bffb452e4b00e2
EiramPP <lastjoseph030@gmail.com>
2024-11-28 11:46:49 +0800

    Updated
    NewUpdate

---
+++ app/Models/Products.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+    protected $primarykey = 'products_id';
+
+
+    protected $fillable = [
+        'product_name',
+        'product_image',
+        'categories_id',
+        'price',
+        'stocks',
+        'is_deleted'
+    ];
+    
+    public function category()
+    {
+        return $this->belongsTo(category::class, 'category_id');
+    }
+
+}
